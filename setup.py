c0fe24a5903795fae6d0f158d4ceb0349af8a56f
Prasannaw Maddipati <prasannamaddipati@harshas-MacBook-Pro.local>
2021-07-07 16:01:20 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src", 
+    version = "0.0.1",
+    description = "It is a wine quality package.",
+    author = "Prasannaw Maddipati",
+    packages = find_packages(),
+    license = "MIT"
+)
